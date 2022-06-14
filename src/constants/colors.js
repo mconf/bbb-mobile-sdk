4e913c26d0106a780304beb079b94b3809968de0
Max Franke <max.franke@acad.pucrs.br>
2022-06-14 14:47:01 -0300

    feat(constants): add colors

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+  blue: '#003399',
+  orange: '#F18700',
+  lightGray100: '#EEF1F4',
+  lightGray200: '#D4DDE4',
+  lightGray300: '#667080',
+  lightGray400: '#28282D',
+};
+
+export default Colors;
