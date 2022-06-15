2e1f66613b25896b2f620cea389ebe6b89c9e6cd
Max Franke <max.franke@acad.pucrs.br>
2022-06-15 13:50:50 -0300

    feat(app): navigator header styles

--- App.js
+++ App.js
@@ -6,14 +6,35 @@ import LoginScreen from './src/screens/LoginScreen';
 import LoadingScreen from './src/screens/LoadingScreen';
 import EmailLoginScreen from './src/screens/LoginScreen/EmailLoginScreen';
 
+import Colors from './src/constants/colors';
+
 const Stack = createNativeStackNavigator();
 
 const App = () => (
   <>
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="EmailLoginScreen" component={EmailLoginScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: Colors.blue },
+          headerTintColor: Colors.white,
+          contentStyle: { backgroundColor: Colors.white },
+        }}
+      >
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{
+            title: 'Login',
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="EmailLoginScreen"
+          component={EmailLoginScreen}
+          options={{
+            title: 'Login Email',
+          }}
+        />
         {/* Acesso federado
           Google login
           Facebook login */}
