2eed0b465ef96155934571af51c151d700c7bfed
Max Franke <max.franke@acad.pucrs.br>
2022-06-22 17:32:51 -0300

    feat(navigation): add navigation drawer

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,9 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
+        plugins: ['react-native-paper/babel', 'react-native-reanimated/plugin'],
       },
     },
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
