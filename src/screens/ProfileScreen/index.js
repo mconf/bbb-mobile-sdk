135a5dfd87c43002e656a06abe493b060494797c
Max Franke <max.franke@acad.pucrs.br>
2022-07-12 14:36:30 -0300

    feat: add BBB App boilerplate

--- src/screens/ProfileScreen/index.js
+++
@@ -1,7 +0,0 @@
-import { Text } from 'react-native';
-
-const ProfileScreen = () => {
-  return <Text>ProfileScreen</Text>;
-};
-
-export default ProfileScreen;
---
+++ src/screens/TestComponents/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+const ContainerView = styled.View`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+`;
+
+export default {
+  ContainerView,
+};
