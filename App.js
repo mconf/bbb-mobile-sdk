0e575a4689c87f64454e824439f6d1611b174f7e
Max Franke <max.franke@acad.pucrs.br>
2022-07-14 12:35:40 -0300

    feat(navigation): add drawer

--- App.js
+++ App.js
@@ -1,11 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import TestComponentsScreen from './src/screens/TestComponents';
+import ClassroomMainScreen from './src/screens/ClassroomMain';
 
-const App = () => (
-  <>
-    <TestComponentsScreen />
-    <StatusBar style="auto" />
-  </>
-);
+const App = () => {
+  const Drawer = createDrawerNavigator();
+  return (
+    <>
+      <NavigationContainer>
+        <Drawer.Navigator
+          drawerContent={(props) => {
+            return (
+              <DrawerContentScrollView {...props}>
+                <DrawerItemList {...props} />
+                <DrawerItem label="Logout" onPress={() => {}} />
+              </DrawerContentScrollView>
+            );
+          }}
+          screenOptions={{
+            contentOptions: {
+              style: {
+                backgroundColor: 'black',
+                flex: 1,
+              },
+            },
+
+            sceneContainerStyle: { backgroundColor: '#06172A' },
+            drawerActiveBackgroundColor: '#003399',
+            drawerActiveTintColor: 'white',
+            headerStyle: { backgroundColor: '#003399' },
+            headerTintColor: 'white',
+            drawerBackgroundColor: '#003399',
+            headerTitleAlign: 'center',
+          }}
+        >
+          <Drawer.Screen
+            name="Main"
+            component={ClassroomMainScreen}
+            options={{
+              title: 'Sala de aula',
+              contentOptions: {
+                style: {
+                  backgroundColor: 'black',
+                  flex: 1,
+                },
+              },
+            }}
+          />
+
+          <Drawer.Screen
+            name="ProfileScreen"
+            component={TestComponentsScreen}
+            options={{
+              title: 'Teste',
+            }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+
+      <StatusBar style="auto" />
+    </>
+  );
+};
 
 export default App;
