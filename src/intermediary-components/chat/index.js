f4fad520b9c0113945e3f6f583e9a85445720dec
Max Franke <max.franke@acad.pucrs.br>
2022-07-15 13:01:18 -0300

    fix(chat): unique key

--- src/intermediary-components/chat/index.js
+++ src/intermediary-components/chat/index.js
@@ -5,12 +5,12 @@ const Chat = (props) => {
   const { messages } = props;
 
   return messages.map((messageObject) => (
-    <>
+    <React.Fragment key={messageObject.author + messageObject.message}>
       <Styled.MessageAuthor>{messageObject.author}</Styled.MessageAuthor>
       <Styled.Card>
         <Styled.MessageContent>{messageObject.message}</Styled.MessageContent>
       </Styled.Card>
-    </>
+    </React.Fragment>
   ));
 };
 
