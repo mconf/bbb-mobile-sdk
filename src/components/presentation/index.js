0d17ab052d70c6e7d091ce7ec76e3002d50012a4
Max Franke <max.franke@acad.pucrs.br>
2022-08-02 17:01:43 -0300

    fix(presentation): resize mode contain

--- src/components/presentation/index.js
+++ src/components/presentation/index.js
@@ -3,7 +3,13 @@ import Styled from './styles';
 const Presentation = (props) => {
   const { source, style } = props;
 
-  return <Styled.PresentationImage source={source} style={style} />;
+  return (
+    <Styled.PresentationImage
+      source={source}
+      style={style}
+      resizeMode="contain"
+    />
+  );
 };
 
 export default Presentation;
