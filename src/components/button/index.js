9b63912eb713e2d5169a988166971f1db7ce6466
Max Franke <max.franke@acad.pucrs.br>
2022-08-04 17:19:15 -0300

    fix(button): use pressable component

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -5,12 +5,7 @@ const PrimaryButton = (props) => {
 
   return (
     <Styled.ButtonOuterContainer>
-      <Styled.ButtonInnerContainer
-        onPress={onPress}
-        variant={variant}
-        // handle "pressed" styled here until styled-components api should let us use inside styles.js
-        style={({ pressed }) => [pressed ? { opacity: 0.75 } : null]}
-      >
+      <Styled.ButtonInnerContainer onPress={onPress} variant={variant}>
         <Styled.ButtonText style={style}>{children}</Styled.ButtonText>
       </Styled.ButtonInnerContainer>
     </Styled.ButtonOuterContainer>
--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,16 +1,24 @@
 import styled from 'styled-components/native';
+import { css } from 'styled-components';
 import Colors from '../../constants/colors';
+import Pressable from '../pressable';
 
 const ButtonOuterContainer = styled.View`
   display: flex;
   margin: 4px;
 `;
 
-const ButtonInnerContainer = styled.Pressable`
-  justify-content: center;
-  display: flex;
-  border-radius: 12px;
-  min-height: 40px;
+const ButtonInnerContainer = styled(Pressable).attrs(() => ({
+  pressStyle: {
+    opacity: 0.8,
+  },
+}))`
+  ${() => css`
+    justify-content: center;
+    display: flex;
+    border-radius: 12px;
+    min-height: 40px;
+  `}
 `;
 
 const ButtonText = styled.Text`
