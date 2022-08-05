744ebcbbfba233f5b76d71141f562b4ed4d76675
Max Franke <max.franke@acad.pucrs.br>
2022-08-05 17:25:36 -0300

    fix(user-participants-screen): use pressable component
    Add pressable effect
    Add actions bar

--- src/screens/user-participants-screen/index.js
+++ src/screens/user-participants-screen/index.js
@@ -1,3 +1,5 @@
+import { Alert } from 'react-native';
+import { useOrientation } from '../../hooks/use-orientation';
 import Styled from './styles';
 
 const UserParticipantsScreen = () => {
@@ -8,14 +10,24 @@ const UserParticipantsScreen = () => {
     'Taz',
     'Lola',
     'Frajola',
+    'Gaguinho',
+    'Pernalonga',
+    'Taz',
+    'Lola',
+    'Frajola',
   ];
+
+  const orientation = useOrientation();
 
   const renderItem = ({ item }) => {
     return (
       <Styled.CardPressable
-        // handle "pressed" styled here until styled-components api should let us use inside styles.js
-        style={({ pressed }) => [pressed ? { opacity: 0.75 } : null]}
-        onPress={() => {}}
+        onPress={() =>
+          Alert.alert(
+            'Currently under development',
+            'This feature will be addressed soon, please check out our github page'
+          )
+        }
       >
         <Styled.UserAvatar userName={item} />
         <Styled.UserName>{item}</Styled.UserName>
@@ -23,7 +35,15 @@ const UserParticipantsScreen = () => {
     );
   };
 
-  return <Styled.FlatList data={userListNames} renderItem={renderItem} />;
+  return (
+    <Styled.ContainerView orientation={orientation}>
+      <Styled.FlatList data={userListNames} renderItem={renderItem} />
+
+      <Styled.ActionsBarContainer orientation={orientation}>
+        <Styled.ActionsBar orientation={orientation} />
+      </Styled.ActionsBarContainer>
+    </Styled.ContainerView>
+  );
 };
 
 export default UserParticipantsScreen;
--- src/screens/user-participants-screen/styles.js
+++ src/screens/user-participants-screen/styles.js
@@ -1,16 +1,41 @@
 import styled from 'styled-components/native';
+import { css } from 'styled-components';
 import userAvatar from '../../components/user-avatar';
 import Colors from '../../constants/colors';
+import actionsBar from '../../components/actions-bar';
+import Pressable from '../../components/pressable';
 
-const CardPressable = styled.Pressable`
-  background-color: ${Colors.white};
+const ContainerView = styled.SafeAreaView`
   width: 100%;
-  min-height: 20px;
-  border-radius: 12px;
-  padding: 12px;
-  flex-direction: row;
+  height: 100%;
+  display: flex;
   align-items: center;
-  margin-bottom: 12px;
+  justify-content: space-around;
+  padding: 10px;
+
+  ${({ orientation }) =>
+    orientation === 'LANDSCAPE' &&
+    `
+    flex-direction: row;
+    justify-content: center;
+  `}
+`;
+
+const CardPressable = styled(Pressable).attrs(() => ({
+  pressStyle: {
+    opacity: 0.8,
+  },
+}))`
+  ${() => css`
+    background-color: ${Colors.white};
+    width: 100%;
+    min-height: 20px;
+    border-radius: 12px;
+    padding: 12px;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 12px;
+  `}
 `;
 
 const UserName = styled.Text`
@@ -21,7 +46,35 @@ const UserName = styled.Text`
 
 const UserAvatar = styled(userAvatar)``;
 const FlatList = styled.FlatList`
+  width: 100%;
+  max-height: 85%;
+  border-radius: 12px;
   padding: 12px;
+  display: flex;
+`;
+
+const ActionsBar = styled(actionsBar)`
+  ${({ orientation }) =>
+    orientation === 'LANDSCAPE' &&
+    `
+      flex-direction: column;
+      display: flex;
+  `}
+`;
+
+const ActionsBarContainer = styled.View`
+  width: 100%;
+  height: 10%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  ${({ orientation }) =>
+    orientation === 'LANDSCAPE' &&
+    `
+      width: 10%;
+      height: 100%;
+  `}
 `;
 
 export default {
@@ -29,4 +82,7 @@ export default {
   UserName,
   CardPressable,
   FlatList,
+  ActionsBar,
+  ActionsBarContainer,
+  ContainerView,
 };
