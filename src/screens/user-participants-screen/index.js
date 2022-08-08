b72197b149ee7a2ece659b5573760a24705cc52a
Max Franke <max.franke@acad.pucrs.br>
2022-08-08 11:39:37 -0300

    feat(user-part-screen): add on press menu

--- src/screens/user-participants-screen/index.js
+++ src/screens/user-participants-screen/index.js
@@ -1,4 +1,6 @@
 import { Alert } from 'react-native';
+import { useState } from 'react';
+import { Menu, Provider } from 'react-native-paper';
 import { useOrientation } from '../../hooks/use-orientation';
 import Styled from './styles';
 
@@ -17,18 +19,25 @@ const UserParticipantsScreen = () => {
     'Frajola',
   ];
 
+  const [showMenu, setShowMenu] = useState(false);
+  const [menuAnchor, setMenuAnchor] = useState({ x: 0, y: 0 });
+
   const orientation = useOrientation();
+
+  const onIconPress = (event) => {
+    const { nativeEvent } = event;
+    const anchor = {
+      x: nativeEvent.pageX,
+      y: nativeEvent.pageY - 150,
+    };
+
+    setMenuAnchor(anchor);
+    setShowMenu(true);
+  };
 
   const renderItem = ({ item }) => {
     return (
-      <Styled.CardPressable
-        onPress={() =>
-          Alert.alert(
-            'Currently under development',
-            'This feature will be addressed soon, please check out our github page'
-          )
-        }
-      >
+      <Styled.CardPressable onPress={onIconPress}>
         <Styled.UserAvatar userName={item} />
         <Styled.UserName>{item}</Styled.UserName>
       </Styled.CardPressable>
@@ -36,13 +45,29 @@ const UserParticipantsScreen = () => {
   };
 
   return (
-    <Styled.ContainerView orientation={orientation}>
-      <Styled.FlatList data={userListNames} renderItem={renderItem} />
-
-      <Styled.ActionsBarContainer orientation={orientation}>
-        <Styled.ActionsBar orientation={orientation} />
-      </Styled.ActionsBarContainer>
-    </Styled.ContainerView>
+    <Provider>
+      <Styled.ContainerView orientation={orientation}>
+        <Styled.FlatList data={userListNames} renderItem={renderItem} />
+        <Menu
+          visible={showMenu}
+          onDismiss={() => setShowMenu(false)}
+          anchor={menuAnchor}
+        >
+          <Menu.Item
+            onPress={() =>
+              Alert.alert(
+                'Currently under development',
+                'This feature will be addressed soon, please check out our github page'
+              )
+            }
+            title="Bate-papo privado"
+          />
+        </Menu>
+        <Styled.ActionsBarContainer orientation={orientation}>
+          <Styled.ActionsBar orientation={orientation} />
+        </Styled.ActionsBarContainer>
+      </Styled.ContainerView>
+    </Provider>
   );
 };
 
