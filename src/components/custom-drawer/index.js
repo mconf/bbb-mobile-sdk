ca899e6ed23ba7fbcd49edd271a5a868c730d2f1
Max Franke <max.franke@acad.pucrs.br>
2022-08-19 18:29:49 -0300

    feat(drawer): add currentUser info

--- src/components/custom-drawer/index.js
+++ src/components/custom-drawer/index.js
@@ -1,32 +1,61 @@
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import Styled from './styles';
 import Colors from '../../constants/colors';
+import Styled from './styles';
 
-const CustomDrawer = (props) => (
-  <Styled.ViewContainer>
-    <DrawerContentScrollView
-      {...props}
-      contentContainerStyle={{ backgroundColor: Colors.blue }}
-    >
-      <Styled.CustomDrawerContainer>
-        <Styled.UserAvatar userName="Patolino" />
-        <Styled.NameUserAvatar>Patolino</Styled.NameUserAvatar>
-      </Styled.CustomDrawerContainer>
-      <Styled.ContainerDrawerItemList>
-        <DrawerItemList {...props} />
-      </Styled.ContainerDrawerItemList>
-    </DrawerContentScrollView>
-    <Styled.ContainerCustomButtons>
-      <Styled.ButtonLeaveContainer onPress={() => {}}>
-        <Styled.ViewLeaveContainer>
-          <Styled.TextLeaveContainer>Sair da sessão</Styled.TextLeaveContainer>
-        </Styled.ViewLeaveContainer>
-      </Styled.ButtonLeaveContainer>
-    </Styled.ContainerCustomButtons>
-  </Styled.ViewContainer>
-);
+const CustomDrawer = (props) => {
+  const currentUserStore = useSelector((state) => state.currentUserCollection);
+
+  // TODO Think a way to avoid this
+  const currentUserObj = Object.values(
+    currentUserStore.currentUserCollection
+  )[0] || {
+    name: 'User not logged in',
+    role: 'VIEWER',
+    color: '#FFFFFF',
+  };
+
+  const handleUserInfo = useCallback(() => {
+    return {
+      name: currentUserObj?.name,
+      role: currentUserObj?.role,
+      color: currentUserObj?.color,
+    };
+  }, [currentUserStore]);
+
+  return (
+    <Styled.ViewContainer>
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{ backgroundColor: Colors.blue }}
+      >
+        <Styled.CustomDrawerContainer>
+          <Styled.UserAvatar
+            userName={handleUserInfo().name}
+            userRole={handleUserInfo().role}
+            userColor={handleUserInfo().color}
+          />
+          <Styled.NameUserAvatar>{handleUserInfo().name}</Styled.NameUserAvatar>
+        </Styled.CustomDrawerContainer>
+        <Styled.ContainerDrawerItemList>
+          <DrawerItemList {...props} />
+        </Styled.ContainerDrawerItemList>
+      </DrawerContentScrollView>
+      <Styled.ContainerCustomButtons>
+        <Styled.ButtonLeaveContainer onPress={() => {}}>
+          <Styled.ViewLeaveContainer>
+            <Styled.TextLeaveContainer>
+              Sair da sessão
+            </Styled.TextLeaveContainer>
+          </Styled.ViewLeaveContainer>
+        </Styled.ButtonLeaveContainer>
+      </Styled.ContainerCustomButtons>
+    </Styled.ViewContainer>
+  );
+};
 
 export default CustomDrawer;
