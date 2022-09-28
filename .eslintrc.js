c0724acbac09444ff808bc06ec2fc958940ca1b5
Max Franke <max.franke@acad.pucrs.br>
2022-09-28 17:20:01 -0300

    fix(ESLint): flow and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
-    'prettier',
     'plugin:flowtype/recommended',
   ],
   parser: '@babel/eslint-parser',
@@ -16,9 +15,8 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', '@babel', 'flowtype'],
+  plugins: ['react', '@babel', 'flowtype'],
   rules: {
-    'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
       'warn',
       {
@@ -33,6 +31,73 @@ module.exports = {
         unnamedComponents: 'arrow-function',
       },
     ],
+
+    // flow rules
+    'flowtype/boolean-style': [
+      2,
+      'boolean',
+    ],
+    'flowtype/define-flow-type': 1,
+    'flowtype/delimiter-dangle': [
+      2,
+      'never',
+    ],
+    'flowtype/generic-spacing': [
+      2,
+      'never',
+    ],
+    'flowtype/interface-id-match': [
+      2,
+      '^([A-Z][a-z0-9]+)+Type$',
+    ],
+    'flowtype/no-mixed': 2,
+    'flowtype/no-primitive-constructor-types': 2,
+    'flowtype/no-types-missing-file-annotation': 0,
+    'flowtype/no-weak-types': 2,
+    'flowtype/object-type-delimiter': [
+      2,
+      'comma',
+    ],
+    'flowtype/require-readonly-react-props': 0,
+    'flowtype/require-valid-file-annotation': 2,
+    'flowtype/semi': [
+      2,
+      'always',
+    ],
+    'flowtype/space-after-type-colon': [
+      2,
+      'always',
+    ],
+    'flowtype/space-before-generic-bracket': [
+      2,
+      'never',
+    ],
+    'flowtype/space-before-type-colon': [
+      2,
+      'never',
+    ],
+    'flowtype/type-id-match': [
+      2,
+      '^([A-Z][a-z0-9]+)+Type$',
+    ],
+    'flowtype/union-intersection-spacing': [
+      2,
+      'always',
+    ],
+    'flowtype/use-flow-type': 1,
+    'flowtype/valid-syntax': 1,
+
+    // review below
+    'flowtype/require-parameter-type': 0,
+    'flowtype/require-return-type': [
+      0,
+      'always',
+      {
+        annotateUndefined: 'never',
+      },
+    ],
+
+    // disable annoying things
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
     'react/jsx-props-no-spreading': 'off',
