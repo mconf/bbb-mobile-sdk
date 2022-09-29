ff160858157f979a2d777a4addf4ad5cb24febdd
Max Franke <max.franke@acad.pucrs.br>
2022-09-29 15:43:49 -0300

    feat(expo-linking): add package

--- src/screens/test-components-screen/index.js
+++ src/screens/test-components-screen/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
+import * as Linking from 'expo-linking';
 import Styled from './styles';
 
 import SocketConnection from '../../components/socket-connection';
 
-const TestComponentsScreen = () => (
-  <SafeAreaView>
-    <Styled.ContainerView>
-      <SocketConnection />
-    </Styled.ContainerView>
-  </SafeAreaView>
-);
+const TestComponentsScreen = () => {
+  const url = Linking.useURL();
+  return (
+    <SafeAreaView>
+      <Styled.ContainerView>
+        <Text>
+          URL:
+          {url}
+        </Text>
+        <SocketConnection />
+      </Styled.ContainerView>
+    </SafeAreaView>
+  );
+};
 
 export default TestComponentsScreen;
