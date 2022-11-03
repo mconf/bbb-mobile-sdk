9c3b1e3fee82a05c4de62ec3433e908e9786db34
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-11-03 17:11:59 -0300

    fix: import combineReducers from @reduxjs/toolkit

--- src/store/redux/store.js
+++ src/store/redux/store.js
@@ -1,5 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 // meteor collections
 import usersReducer from './slices/users';
 import meetingReducer from './slices/meeting';
