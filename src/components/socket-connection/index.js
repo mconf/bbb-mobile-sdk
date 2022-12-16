7b9d006265806e01fe702210fe0524e0940607c8
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-12-16 17:43:57 -0300

    fix: add loading skeleton for the presentation

--- src/components/socket-connection/index.js
+++ src/components/socket-connection/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
