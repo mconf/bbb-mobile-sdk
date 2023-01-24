bafce67d75e030e3622b5c3304fbd67db6cc2dc8
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2023-01-24 14:51:17 -0300

    refactor(socket): address linting issues, use ES6 exports in utils
    Also add a FIXME to remind us to re-do the "random" generators

--- src/components/socket-connection/utils.js
+++ src/components/socket-connection/utils.js
@@ -1,38 +1,41 @@
+// FIXME review random generators - definitely not ideal prlanzarin Jan 24 2023
+const DIGITS = '1234567890';
+const ALPHANUMERIC = 'abcdefghijklmnopqrstuvwxyz1234567890';
+const ALPHANUMERIC_WITH_CAPS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+const HEX = 'abcdef1234567890';
+
 const randomFromString = (chars, n) => {
   let result = '';
-  const charsLength = chars.length;
+  const charsLength = typeof n === 'number' && n > 0 ? n : chars.length;
 
-  for (let i = 0; i < charsLength; i++) {
+  for (let i = 0; i < charsLength; i += 1) {
     result += chars.charAt(Math.floor(Math.random() * charsLength));
   }
 
   return result;
-}
+};
 
-const DIGITS = "1234567890";
-const ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyz1234567890";
-const ALPHANUMERIC_WITH_CAPS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-const HEX = "abcdef1234567890";
+const getRandomDigits = (n) => randomFromString(DIGITS, n);
+const getRandomAlphanumeric = (n) => randomFromString(ALPHANUMERIC, n);
+const getRandomAlphanumericWithCaps = (n) => randomFromString(ALPHANUMERIC_WITH_CAPS, n);
+const getRandomHex = (n) => randomFromString(HEX, n);
+const decodeMessage = (msg) => {
+  let msgObj = {};
 
-const MainWebSocketUtil = {
-  getRandomDigits: (n) => randomFromString(DIGITS, n),
-  getRandomAlphanumeric: (n) => randomFromString(ALPHANUMERIC, n),
-  getRandomAlphanumericWithCaps: (n) => randomFromString(ALPHANUMERIC_WITH_CAPS, n),
-  getRandomHex: (n) => randomFromString(HEX, n),
+  try {
+    msgObj = JSON.parse(msg);
+    msgObj = JSON.parse(msgObj[0]);
+  } catch {
+    msgObj = {};
+  }
 
-  decodeMessage: (msg) => {
-    let msgObj = {};
+  return msgObj;
+};
 
-    try {
-      msgObj = JSON.parse(msg);
-      msgObj = JSON.parse(msgObj[0]);
-    } catch {
-      msgObj = {}
-    }
-
-    return msgObj;
-  },
-
-}
-
-module.exports = MainWebSocketUtil;
+export {
+  getRandomDigits,
+  getRandomAlphanumeric,
+  getRandomAlphanumericWithCaps,
+  getRandomHex,
+  decodeMessage,
+};
