9c4fc527a569bdda2e72510a1eb716b27c8bb1c9
Lucas Zawacki <lfzawacki@gmail.com>
2023-03-22 11:54:54 -0300

    Script now adds missing locales as empty strings, update ES

--- generate_locales.sh
+++ generate_locales.sh
@@ -12,10 +12,20 @@ LOCALE_FILE=$LOCALE_PATH/$1.json
 echo "{" > $LOCALE_FILE
 grep -Fw -f $KEYS_FILE "$HTML5_LOCALE_PATH/$1.json" >> $LOCALE_FILE
 
-cat $LOCALE_FILE | tail -1 | grep ",$"
-
-if [ "$?" = "0" ]; then
-  truncate -s -2 $LOCALE_FILE
+cat $LOCALE_FILE | tail -1 | grep ",$" -q
+if [ "$?" = "1" ]; then
+  truncate -s -1 $LOCALE_FILE
+  echo ',' >> $LOCALE_FILE
 fi;
 
-echo "}" >> $LOCALE_FILE
+cat $LOCALE_FILE | grep "(\".*\"):" -oEi > $KEYS_FILE
+
+grep -Fwv -f $KEYS_FILE $BASE_FILE | grep "(\".*\"):" -oEi | sed -e 's/.*/    & "",/' >> $LOCALE_FILE
+
+cat $LOCALE_FILE | tail -1 | grep ",$" -q
+if [ "$?" = "0" ]; then
+  truncate -s -2 $LOCALE_FILE
+  echo '' >> $LOCALE_FILE
+fi;
+
+echo -n "}" >> $LOCALE_FILE
