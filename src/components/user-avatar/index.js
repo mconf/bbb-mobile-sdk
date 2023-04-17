27eee7dd01d35948fce7ad649cdfd9c0832f4cc3
Max Franke <max.franke@acad.pucrs.br>
2023-04-17 15:49:35 -0300

    feat(user-avatar): allow userRole

--- src/components/user-avatar/index.js
+++ src/components/user-avatar/index.js
@@ -1,11 +1,24 @@
 import Styled from './styles';
 
 const UserAvatar = (props) => {
-  const { userName, userRole, userColor } = props;
+  const {
+    userName, userRole, userColor, userImage, isTalking
+  } = props;
 
   return (
-    <Styled.Background userRole={userRole} userColor={userColor}>
-      <Styled.UserName>{userName?.substring(0, 2)}</Styled.UserName>
+    <Styled.Background
+      userRole={userRole}
+      userColor={userColor}
+      userImage={userImage}
+      isTalking={isTalking}
+    >
+      {userImage
+        ? (
+          <Styled.ImageContainer userRole={userRole}>
+            <Styled.ImageBackground source={{ uri: userImage }} userRole={userRole} />
+          </Styled.ImageContainer>
+        )
+        : <Styled.UserName>{userName?.substring(0, 2)}</Styled.UserName>}
     </Styled.Background>
   );
 };
