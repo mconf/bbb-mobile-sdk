5437ab27a8ac9efffa5456ef4ecfbc28f452cb3b
Max <max.franke@acad.pucrs.br>
2023-06-21 15:52:42 -0300

    feat(main-conference-screen): add skeleton loading
    Rename screen
    Add skeleton loading

--- src/screens/main-conference-screen/index.js
+++ src/screens/main-conference-screen/index.js
@@ -1,10 +1,19 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import withPortal from '../../components/high-order/with-portal';
 import VideoGrid from '../../components/video/video-grid';
 import BottomSheetActionsBar from '../../components/actions-bar/bottom-sheet-actions-bar';
 import ChatPopupList from '../../components/chat/chat-popup';
 import Styled from './styles';
 
-const ClassroomMainScreen = () => {
+const MainConferenceScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const loggingIn = useSelector((state) => state.client.sessionState.loggingIn);
+
+  useEffect(() => {
+    setIsLoading(loggingIn);
+  }, [loggingIn]);
+
   /* view components */
   const renderGridLayout = () => {
     return (
@@ -16,7 +25,13 @@ const ClassroomMainScreen = () => {
     );
   };
 
+  if (isLoading) {
+    return (
+      <Styled.GridItemSkeletonLoading />
+    );
+  }
+
   return renderGridLayout();
 };
 
-export default withPortal(ClassroomMainScreen);
+export default withPortal(MainConferenceScreen);
