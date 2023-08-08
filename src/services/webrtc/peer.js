cfcd074f2662ec66a103bcc6063f1861b98b28d7
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2023-08-08 12:11:04 -0300

    feat(audio): add support for meta_transparent-listen-only

--- src/services/webrtc/peer.js
+++ src/services/webrtc/peer.js
@@ -399,12 +399,25 @@ export default class WebRtcPeer extends EventEmitter2 {
     }
 
     return this.peerConnection.setRemoteDescription(offer)
+      .then(async () => {
+        if (this.mode === 'sendonly' || this.mode === 'sendrecv') {
+          await this.mediaStreamFactory();
+
+          if (this.videoStream) {
+            this.peerConnection.addStream(this.videoStream);
+          }
+
+          if (this.audioStream) {
+            this.peerConnection.addStream(this.audioStream);
+          }
+        }
+      })
       .then(() => {
         this.logger.debug({
           logCode: 'BBB::WebRtcPeer::processOffer::setRemoteDescription',
           extraInfo: this._logMetadata,
         }, 'BBB::WebRtcPeer::processOffer - Remote description set');
-        return this.peerConnection.createAnswer()
+        return this.peerConnection.createAnswer();
       })
       .then((answer) => {
         this.logger.debug({
--- src/store/redux/slices/voice-users.js
+++ src/store/redux/slices/voice-users.js
@@ -2,7 +2,12 @@ import { createSlice, createAsyncThunk, createSelector } from '@reduxjs/toolkit'
 import { Platform, PermissionsAndroid } from 'react-native';
 import AudioManager from '../../../services/webrtc/audio-manager';
 import { setLoggedIn } from './wide-app/client';
-import { addMeeting, selectMeeting, selectLockSettingsProp } from './meeting';
+import {
+  addMeeting,
+  selectMeeting,
+  selectLockSettingsProp,
+  selectMetadata,
+} from './meeting';
 import { addCurrentUser, selectCurrentUser, isLocked } from './current-user';
 import { setAudioError } from './wide-app/audio';
 import logger from '../../../services/logger';
@@ -145,6 +150,7 @@ const joinAudio = createAsyncThunk(
     const state = thunkAPI.getState();
     const muteOnStart = selectMeeting(state)?.voiceProp?.muteOnStart;
     const micDisabled = selectLockSettingsProp(state, 'disableMic') && isLocked(state);
+    const transparentListenOnly = selectMetadata(state, 'transparent-listen-only');
 
     if (Platform.OS === 'android' && Platform.Version >= ANDROID_SDK_MIN_BTCONNECT) {
       const checkStatus = await PermissionsAndroid.check(
@@ -168,6 +174,7 @@ const joinAudio = createAsyncThunk(
     return AudioManager.joinMicrophone({
       muted: muteOnStart,
       isListenOnly: micDisabled,
+      transparentListenOnly,
     }).catch((error) => {
       logger.error({
         logCode: 'audio_publish_failure',
