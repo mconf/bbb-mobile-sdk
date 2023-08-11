ee8b317cf38d4cd8e35fd25541b0c795b41aa7d0
Max Franke <max.franke@acad.pucrs.br>
2023-08-11 16:44:53 -0300

    refactor(high-order): move detailedInfo actions to portal component

--- src/components/high-order/with-portal/index.js
+++ src/components/high-order/with-portal/index.js
@@ -1,11 +1,20 @@
+import { Pressable } from 'react-native';
 import BottomSheetChat from '../../chat/bottom-sheet-chat';
 import NotificationBar from '../../notification-bar';
 import BottomSheetActionsBar from '../../actions-bar/bottom-sheet-actions-bar';
+import { trigDetailedInfo } from '../../../store/redux/slices/wide-app/layout';
+import { store } from '../../../store/redux/store';
 
 const withPortal = (Component) => {
+  const handleDispatchDetailedInfo = () => {
+    return store.dispatch(trigDetailedInfo());
+  };
+
   return (props) => (
     <>
-      <Component {...props} />
+      <Pressable onPress={handleDispatchDetailedInfo}>
+        <Component {...props} />
+      </Pressable>
       <BottomSheetActionsBar />
       <NotificationBar />
       <BottomSheetChat />
