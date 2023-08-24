29ad29dea80b3974dbc1119dc92032942dc59d81
Max Franke <max.franke@acad.pucrs.br>
2023-08-24 16:26:35 -0300

    fix(previous-polls): cant scroll

--- src/screens/poll-screen/previous-polls-screen/styles.js
+++ src/screens/poll-screen/previous-polls-screen/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components/native';
-import Colors from '../../../constants/colors';
 
 const Title = styled.Text`
   font-size: 24px;
@@ -21,7 +20,6 @@ const ContainerViewPadding = styled.View`
 `;
 
 const ContainerPollCard = styled.ScrollView`
-  background-color: ${Colors.white};
   width: 100%;
   border-radius: 12px;
   display: flex;
