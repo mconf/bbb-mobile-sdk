4e749f813af7b1ebe645116b2e816d260ac2f684
AtilaU19 <atilauebel2000@gmail.com>
2023-09-12 16:06:32 -0300

    feat(main-screen): shows who the presenter is

--- src/components/user-avatar/index.js
+++ src/components/user-avatar/index.js
@@ -1,9 +1,16 @@
+import { useSelector } from 'react-redux';
+import { selectUsers } from '../../store/redux/slices/users';
 import Styled from './styles';
 
 const UserAvatar = (props) => {
   const {
-    userName, userRole, userColor, userImage, isTalking
+    userName, userRole, userColor, userImage, isTalking, userId,
   } = props;
+
+  const currentUserObj = useSelector(selectUsers);
+  const presenterUser = currentUserObj.find((user) => user.presenter === true);
+
+  const shouldRenderPresenterIcon = presenterUser && presenterUser.userId === userId;
 
   return (
     <Styled.Background
@@ -19,6 +26,12 @@ const UserAvatar = (props) => {
           </Styled.ImageContainer>
         )
         : <Styled.UserName>{userName?.substring(0, 2)}</Styled.UserName>}
+      {shouldRenderPresenterIcon && (
+        <Styled.PresenterIcon
+          icon="presentation"
+          size={13}
+        />
+      )}
     </Styled.Background>
   );
 };
