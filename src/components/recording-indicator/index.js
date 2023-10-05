06225a91392f5a9ecf1184078d13ae556861f5db
Max Franke <max.franke@acad.pucrs.br>
2023-10-05 17:37:58 -0300

    feat(recording-button): add pulse animation

--- src/components/recording-indicator/index.js
+++ src/components/recording-indicator/index.js
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import Svg, { G, Circle } from 'react-native-svg';
-import { Pressable, } from 'react-native';
+import { Pressable, Animated } from 'react-native';
 import { useDispatch } from 'react-redux';
+import { useFocusEffect } from '@react-navigation/core';
 import Colors from '../../constants/colors';
 import Styled from './styles';
 import usePrevious from '../../hooks/use-previous';
@@ -14,6 +15,7 @@ const RecordingIndicator = (props) => {
   const previousRecording = usePrevious(recording);
 
   const dispatch = useDispatch();
+  const anim = useRef(new Animated.Value(1));
 
   const handleOpenModal = () => {
     dispatch(setActiveModal('recording-status'));
@@ -32,20 +34,41 @@ const RecordingIndicator = (props) => {
     }
   }, [recording]);
 
+  useFocusEffect(
+    useCallback(() => {
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(anim.current, {
+            toValue: 1.1,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+          Animated.timing(anim.current, {
+            toValue: 1,
+            duration: 500,
+            useNativeDriver: true,
+          }),
+        ])
+      ).start();
+    }, [])
+  );
+
   if (!recordMeeting?.record) return null;
 
   return (
     <Styled.RecordingIndicatorIcon titleMargin={recording}>
       <Pressable onPress={handleOpenModal}>
-        <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-          <G
-            fill={recording ? Colors.orange : Colors.white}
-            stroke={recording ? Colors.orange : Colors.white}
-          >
-            <Circle cx={10} cy={10} r={9} fill="none" />
-            <Circle cx={10} cy={10} r={recording ? '5' : '4'} />
-          </G>
-        </Svg>
+        <Animated.View style={{ transform: [{ scale: recording ? anim.current : 1 }] }}>
+          <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <G
+              fill={recording ? Colors.orange : Colors.white}
+              stroke={recording ? Colors.orange : Colors.white}
+            >
+              <Circle cx={10} cy={10} r={9} fill="none" />
+              <Circle cx={10} cy={10} r={4} />
+            </G>
+          </Svg>
+        </Animated.View>
       </Pressable>
     </Styled.RecordingIndicatorIcon>
   );
