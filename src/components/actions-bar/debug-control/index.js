b287e5e5e6e597d3c37c1c57228620c26984b8b3
Max Franke <max.franke@acad.pucrs.br>
2023-10-11 17:30:26 -0300

    feat(debug): add debug control in actions bar

---
+++ src/components/actions-bar/debug-control/index.js
@@ -0,0 +1,42 @@
+import React, { useState, useCallback } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { show, hide } from '../../../store/redux/slices/wide-app/debug';
+import Colors from '../../../constants/colors';
+import Styled from './styles';
+
+const DebugControl = () => {
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+
+  const [isSwitchOn, setIsSwitchOn] = useState(false);
+
+  useFocusEffect(useCallback(() => {
+    if (isSwitchOn) {
+      dispatch(show());
+    } else {
+      dispatch(hide());
+    }
+  }, [isSwitchOn]));
+
+  const onToggleSwitch = () => {
+    setIsSwitchOn((prevValue) => !prevValue);
+  };
+
+  return (
+    <Styled.DebugContainer>
+      <Styled.DebugIcon name="wrench" size={24} color="white" />
+      <Styled.DebugText>
+        {t('mobileSdk.actionsBar.debug.label')}
+      </Styled.DebugText>
+      <Styled.DebugModeSwitch
+        value={isSwitchOn}
+        onValueChange={onToggleSwitch}
+        color={Colors.blue}
+      />
+    </Styled.DebugContainer>
+  );
+};
+
+export default DebugControl;
---
+++ src/components/actions-bar/debug-control/styles.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+import { Switch } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
+import Colors from '../../../constants/colors';
+
+const DebugContainer = styled.View`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: ${Colors.lightGray300};
+  border-radius: 12px;
+  padding: 12px;
+  width: 95%;
+  gap: 12px;
+`;
+
+const DebugIcon = styled(FontAwesome)`
+  padding: 4px;
+`;
+
+const DebugText = styled.Text`
+  font-size: 16px;
+  color: ${Colors.lightGray100}
+  text-align: center;
+  flex: 1;
+`;
+
+const DebugModeSwitch = styled(Switch)`
+`;
+
+export default {
+  DebugModeSwitch,
+  DebugContainer,
+  DebugText,
+  DebugIcon
+};
