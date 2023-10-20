c8035a613340d2ae310c7216813a79d882c3ccf9
Max Franke <max.franke@acad.pucrs.br>
2023-10-20 15:52:59 -0300

    refactor(previous-polls): refactor using the new figma designs

--- src/screens/poll-screen/previous-polls-screen/poll-card/index.js
+++ src/screens/poll-screen/previous-polls-screen/poll-card/index.js
@@ -1,8 +1,9 @@
 import { View } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import Styled from './styles';
 import { trigDetailedInfo } from '../../../../store/redux/slices/wide-app/layout';
+import ActivityBar from '../../../../components/activity-bar';
+import Styled from './styles';
 
 const PreviousPollCard = (props) => {
   const { pollObj } = props;
@@ -11,37 +12,53 @@ const PreviousPollCard = (props) => {
   const dispatch = useDispatch();
   const noPollLocale = pollObj?.questionType === 'CUSTOM' || pollObj?.questionType === 'R-';
 
-  return (
-    <Styled.ContainerPollCard onPress={() => dispatch(trigDetailedInfo())}>
-      <Styled.QuestionText>
-        {pollObj.questionText === ''
-          ? t('mobileSdk.poll.noQuestionTextProvided')
-          : pollObj.questionText}
-      </Styled.QuestionText>
-      <Styled.TimestampText>
-        {`${String(timestamp.getHours()).padStart(2, '0')}:${String(
-          timestamp.getMinutes()
-        ).padStart(2, '0')}`}
-      </Styled.TimestampText>
-      {pollObj.answers.map((answer) => (
-        <View key={answer.id}>
-          <Styled.AnswerContainer>
-            <Styled.KeyText>
-              {noPollLocale ? answer.key : t(`app.poll.answer.${answer.key}`.toLowerCase())}
-              {' '}
-              :
-              {' '}
-            </Styled.KeyText>
+  const renderAnswers = () => (
+    pollObj.answers.map((answer) => (
+      <View key={answer.id}>
+        <Styled.AnswerContainer>
+          <Styled.LabelContainer>
             <Styled.KeyText>
               {(pollObj.numResponders === 0
                 ? 0
                 : (answer.numVotes / pollObj.numResponders) * 100).toFixed(0)}
               %
             </Styled.KeyText>
-          </Styled.AnswerContainer>
-        </View>
-      ))}
-    </Styled.ContainerPollCard>
+            <Styled.KeyText>
+              {noPollLocale ? answer.key : t(`app.poll.answer.${answer.key}`.toLowerCase())}
+            </Styled.KeyText>
+          </Styled.LabelContainer>
+          <ActivityBar
+            width={`${pollObj.numResponders === 0
+              ? 0
+              : (((answer.numVotes / pollObj.numResponders) * 100).toFixed(0))}%`}
+          />
+        </Styled.AnswerContainer>
+      </View>
+    ))
+  );
+
+  return (
+    <View>
+      <Styled.ContainerPollCard onPress={() => dispatch(trigDetailedInfo())}>
+        <Styled.QuestionText>
+          {pollObj.questionText === ''
+            ? t('mobileSdk.poll.noQuestionTextProvided')
+            : pollObj.questionText}
+        </Styled.QuestionText>
+        {renderAnswers()}
+        <Styled.CustomDivider />
+        <Styled.PollInfoLabelContainer>
+          <Styled.PollInfoText>{`${pollObj.numResponders} / ${pollObj.numRespondents}`}</Styled.PollInfoText>
+          <Styled.PollInfoText>
+            {`${String(timestamp.getHours()).padStart(2, '0')}:${String(
+              timestamp.getMinutes()
+            ).padStart(2, '0')}`}
+          </Styled.PollInfoText>
+        </Styled.PollInfoLabelContainer>
+        <Styled.BlankSpaceForButton />
+      </Styled.ContainerPollCard>
+      <Styled.PressableButton>{t('mobileSdk.poll.previousPolls.publishedLabel')}</Styled.PressableButton>
+    </View>
   );
 };
 
