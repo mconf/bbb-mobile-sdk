f3209a31d72a0caddccd594cb2ad49c3c65f7a19
Max Franke <max.franke@acad.pucrs.br>
2023-10-31 12:28:51 -0300

    feat(tag): add component

---
+++ src/components/tag/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+import Colors from '../../constants/colors';
+
+const Container = styled.View`
+  padding: 4px 8px;
+  border-radius: 12px;
+  background-color: ${Colors.orange}
+`;
+
+const Text = styled.Text`
+  color: ${Colors.white}
+  font-size: 14px;
+  font-weight: 800;
+
+`;
+
+export default {
+  Container,
+  Text,
+};
