ce4b35366f64d8be3372d1cad087bb98d62ca793
germanocaumo <germanocaumo@gmail.com>
2023-12-05 12:04:22 +0000

    feat(whiteboard): presentationOnly joinUrl in whiteboard screen

--- src/screens/whiteboard-screen/index.js
+++ src/screens/whiteboard-screen/index.js
@@ -1,10 +1,50 @@
 import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useSelector } from 'react-redux';
+import {
+  useEffect, useRef, useState
+} from 'react';
+import axios from 'axios';
+import logger from '../../services/logger';
 
 const WhiteboardScreen = () => {
+  const joinUrl = useSelector((state) => state.client.meetingData.joinUrl);
+
+  const [presentationOnlyJoinUrl, setPresentationOnlyJoinUrl] = useState('');
+  const webViewRef = useRef();
+
+  useEffect(() => {
+    const url = new URL(joinUrl);
+    const getJoinUrlWithEnforceLayout = `https://${url.host}/bigbluebutton/api/getJoinUrl?sessionToken=${url.searchParams.get('sessionToken')}&enforceLayout=presentationOnly`;
+
+    axios.get(getJoinUrlWithEnforceLayout).then((response) => {
+      const apiResponse = response.data.response;
+      if (apiResponse.returncode === 'SUCCESS') {
+        setPresentationOnlyJoinUrl(apiResponse.url);
+      }
+    }).catch((e) => {
+      logger.warn({
+        logCode: 'app_whiteboard_get_enforce_layout_joinUrl_error',
+        extraInfo: {
+          errorName: e.name,
+          errorMessage: e.message,
+        },
+      }, `Unable to get enforceLayout Join URL: ${e.message}`);
+    });
+
+    return () => {
+    };
+  }, []);
+
+  useEffect(() => {
+    webViewRef.current.reload();
+  }, [presentationOnlyJoinUrl]);
   return (
     <View style={{ flex: 1 }}>
-      <WebView source={{ uri: 'https://www.tldraw.com/' }} />
+      <WebView
+        ref={(ref) => { webViewRef.current = ref; }}
+        source={{ uri: presentationOnlyJoinUrl }}
+      />
     </View>
   );
 };
