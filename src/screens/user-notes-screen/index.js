8172a508b2e17d44ca6ea9d646e23d59052f7749
Max Franke <max.franke@acad.pucrs.br>
2024-01-23 18:26:17 -0300

    fix(user-notes): add a padding to trigger actions bar

--- src/screens/user-notes-screen/index.js
+++ src/screens/user-notes-screen/index.js
@@ -1,7 +1,8 @@
-import { View } from 'react-native';
+import { View, Pressable } from 'react-native';
 import { WebView } from 'react-native-webview';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
+import { trigDetailedInfo } from '../../store/redux/slices/wide-app/layout';
 import makeCall from '../../services/api/makeCall';
 import { selectPadSession } from '../../store/redux/slices/pads-sessions';
 import ScreenWrapper from '../../components/screen-wrapper';
@@ -11,6 +12,7 @@ const UserNotesScreen = () => {
   const padSession = useSelector(selectPadSession);
   const host = useSelector((state) => state.client.meetingData.host);
   const [padId, setPadId] = useState('');
+  const dispatch = useDispatch();
 
   const url = `https://${host}/pad/auth_session?padName=${padId}&sessionID=${padSession}&lang=pt-br&rtl=false&sessionToken=${sessionToken}`;
 
@@ -33,8 +35,9 @@ const UserNotesScreen = () => {
 
   return (
     <ScreenWrapper alwaysShowActionBar>
-      <View style={{ flex: 1, paddingBottom: 100 }}>
+      <View style={{ flex: 1 }}>
         <WebView source={{ uri: url }} />
+        <Pressable style={{ height: 100 }} onPress={() => dispatch(trigDetailedInfo())} />
       </View>
     </ScreenWrapper>
   );
