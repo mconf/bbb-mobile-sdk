aa557ab6fb3961048edb64980f1703beff7f582e
Max <max.franke@acad.pucrs.br>
2024-02-28 08:51:29 -0300

    refactor(cant-create-poll-modal): use RNpaper button

--- src/screens/poll-screen/modals/cant-create-poll/styles.js
+++ src/screens/poll-screen/modals/cant-create-poll/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
+import { Button } from 'react-native-paper';
 import Colors from '../../../../constants/colors';
-import button from '../../../../components/button';
 
 const Container = styled.View`
   display: flex;
@@ -24,25 +24,46 @@ const TitleDesc = styled.Text`
   color: ${Colors.lightGray300}
 `;
 
-const MakePresenterButton = styled(button)`
-  background-color: ${Colors.blue}
-  color: ${Colors.white};
-  font-size: 16px;
-  font-weight: 400;
-  padding: 12px;
-  border-radius: 12px;
-  width: 100%;
+const ButtonCreate = styled(Button)`
 `;
 
-const OkButton = styled(button)`
-  background-color: ${Colors.orange}
-  color: ${Colors.white};
-  font-size: 16px;
-  font-weight: 400;
-  padding: 12px;
-  border-radius: 12px;
-  width: 100%;
-`;
+const MakePresenterButton = ({
+  onPress, children, disabled, onPressDisabled
+}) => {
+  return (
+    <ButtonCreate
+      mode="contained"
+      onPress={disabled ? onPressDisabled : onPress}
+      buttonColor={disabled ? Colors.lightGray300 : Colors.orange}
+      textColor={Colors.white}
+      labelStyle={{
+        fontSize: 18,
+        fontWeight: 500,
+      }}
+    >
+      {children}
+    </ButtonCreate>
+  );
+};
+
+const OkButton = ({
+  onPress, children, disabled, onPressDisabled
+}) => {
+  return (
+    <ButtonCreate
+      mode="contained"
+      onPress={disabled ? onPressDisabled : onPress}
+      buttonColor={disabled ? Colors.lightGray300 : Colors.orange}
+      textColor={Colors.white}
+      labelStyle={{
+        fontSize: 18,
+        fontWeight: 500,
+      }}
+    >
+      {children}
+    </ButtonCreate>
+  );
+};
 
 const ButtonContainer = styled.View`
   width: 100%;
