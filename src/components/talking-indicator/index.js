74e28b9f72cb6273d9be30b9b4075b923123dbb2
Max <max.franke@acad.pucrs.br>
2024-02-28 06:58:43 -0300

    feat(talking-indicator): only render when !detailedInfo

--- src/components/talking-indicator/index.js
+++ src/components/talking-indicator/index.js
@@ -2,12 +2,18 @@ import { useSelector } from 'react-redux';
 import Styled from './styles';
 
 const TalkingIndicator = () => {
+  const detailedInfo = useSelector((state) => state.layout.detailedInfo);
   const VoiceUsersCollection = useSelector(
     (state) => state.voiceUsersCollection.voiceUsersCollection
   );
   const callersTalking = Object.values(VoiceUsersCollection)
     .filter((call) => call.talking)
     .map((call) => ({ callerName: call.callerName, voiceUserId: call.voiceUserId }));
+
+  if (detailedInfo) {
+    return;
+  }
+
   return (
     <Styled.Container>
       {callersTalking.map((userObj) => (
