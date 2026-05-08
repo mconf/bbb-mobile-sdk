3a50d0cf7a3948fde5f414368b9f4a18461c85cf
matheus-lagreca <matheus.lagreca@gmail.com>
2026-05-08 17:18:38 -0300

    fix: whiteboard browser outdated error

--- src/screens/whiteboard-screen/index.js
+++ src/screens/whiteboard-screen/index.js
@@ -7,9 +7,13 @@ import {
 import axios from 'axios';
 import { setAdjustPan, setAdjustResize } from 'rn-android-keyboard-adjust';
 import logger from '../../services/logger';
+import useMeetingSettings from '../../graphql/local-states/useMeetingSettings';
 
 const WhiteboardScreen = () => {
   const joinUrl = useSelector((state) => state.client.meetingData.joinUrl);
+  const host = useSelector((state) => state.client.meetingData.host);
+  const sessionToken = useSelector((state) => state.client.meetingData.sessionToken);
+  const [meetingSettings] = useMeetingSettings()
 
   const [presentationOnlyJoinUrl, setPresentationOnlyJoinUrl] = useState('');
   const webViewRef = useRef();
@@ -21,8 +25,7 @@ const WhiteboardScreen = () => {
       return;
     }
 
-    const url = new URL(joinUrl);
-    const getJoinUrlWithEnforceLayout = `https://${url.host}/bigbluebutton/api/getJoinUrl?sessionToken=${url.searchParams?.get('sessionToken')}&enforceLayout=PRESENTATION_ONLY`;
+    const getJoinUrlWithEnforceLayout = `https://${host}/bigbluebutton/api/getJoinUrl?sessionToken=${sessionToken}&enforceLayout=PRESENTATION_ONLY`;
 
     axios.get(getJoinUrlWithEnforceLayout).then((response) => {
       const apiResponse = response.data.response;
@@ -58,6 +61,7 @@ const WhiteboardScreen = () => {
       <WebView
         ref={(ref) => { webViewRef.current = ref; }}
         source={{ uri: presentationOnlyJoinUrl }}
+        userAgent="Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
         javaScriptEnabled
         sharedCookiesEnabled
         thirdPartyCookiesEnabled
