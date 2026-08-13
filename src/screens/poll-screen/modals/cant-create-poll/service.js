30088b9deeb47c374c7d37f9a47533c7c4a6ed1a
matheus-lagreca <matheus.lagreca@gmail.com>
2026-08-13 14:45:57 -0300

    chore(breakouts): cleanup polls
    Polls currently are not being used on the breakouts

--- src/screens/poll-screen/modals/cant-create-poll/service.js
+++
@@ -1,9 +0,0 @@
-import makeCall from '../../../../services/api/makeCall';
-
-const assignPresenter = (currentUserId) => {
-  makeCall('assignPresenter', currentUserId);
-};
-
-export {
-  assignPresenter,
-};
